802dde76376be1e7e3521685fd72cdb37f41e956
Rhiann B <rhiann@caerphilly.observer>
2019-12-03 16:37:50 +0000

    added css and extra requests

--- src/numbers_app/numbers_app/settings.py
+++ src/numbers_app/numbers_app/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = '/static/' # the path in url
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
